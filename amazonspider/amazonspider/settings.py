2255ce481242fd8ecdbf766c9a69034ca8af5ecf
leaeb <61868414+leaeb@users.noreply.github.com>
2020-12-18 23:16:28 +0100

    two candles, only US

--- amazonspider/amazonspider/settings.py
+++ amazonspider/amazonspider/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'amazonspider.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'amazonspider (+http://www.yourdomain.com)'
+USER_AGENT = 'amazonspider (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
--- amazonspider/amazonspider/spiders/amazon_spider.py
+++ amazonspider/amazonspider/spiders/amazon_spider.py
@@ -1,38 +1,52 @@
 import scrapy
 import requests
 from scrapy.http import Request
+from time import sleep
+import csv
+from dateutil import parser as dateparser
+from dateutil.parser import parse
 
-URL = "https://www.amazon.com/Yankee-Candle-Large-Lemon-Lavender/product-reviews/B000WQZ5PC/ref=cm_cr_arp_d_viewpnt_rgt?ie=UTF8&reviewerType=all_reviews&filterByStar=critical&pageNumber=%d"   
-starting_number = 2
-number_of_pages = 2  
+URL_yc = "https://www.amazon.com/Yankee-Candle-Large-Lemon-Lavender/product-reviews/B000WQZ5PC/ref=cm_cr_arp_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber=%d"   
+starting_number_yc = 1
+number_of_pages_yc = 500
+URL_cp = "https://www.amazon.com/Chesapeake-Bay-Candle-Laughter-Cranberry/product-reviews/B01N0RSCW2/ref=cm_cr_arp_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber=%d"
+starting_number_cp = 1
+number_of_pages_cp = 438
+
 
 class AmazonSpiderSpider(scrapy.Spider):
     name = 'amazon_spider'
     allowed_domains = ['amazon.com']
-    start_urls = [URL % starting_number]
+    start_urls = [URL_yc % starting_number_yc, URL_cp % starting_number_cp]
     def __init__(self):
-        self.page_number = starting_number
+        self.page_number = starting_number_yc
 
     def start_requests(self):
-        
-        for i in range (self.page_number, number_of_pages, +1):
-            yield Request(url = URL % i, callback=self.parse)
+        for i in range (self.page_number, number_of_pages_yc, +1):
+            yield scrapy.Request(URL_yc % i, self.parse)
+        for i in range (self.page_number, number_of_pages_cp, +1):
+            yield scrapy.Request(URL_cp % i, self.parse)
 
     def parse(self, response):
         for row in response.css('div.review'):
             item = {}
 
-            item['author'] = row.css('span.a-profile-name::text').extract_first()
+            #item['author'] = row.css('span.a-profile-name::text').extract_first()
 
             rating = row.css('i.review-rating > span::text').extract_first().strip().split(' ')[0]
             item['rating'] = int(float(rating.strip().replace(',', '.')))
 
-            item['title'] = row.css('span.review-title > span::text').extract_first()
-            created_date = row.css('span.review-date::text').extract_first().strip()
-            item['created_date'] = created_date
+            #item['title'] = row.css('span.review-title > span::text').extract_first()
+            created_date = row.css('span.review-date::text').extract_first().strip().split('on ')[-1]
+            item['year'] = dateparser.parse(created_date).strftime('%Y')
+            item['month'] = dateparser.parse(created_date).strftime('%m')
 
-            review_content = row.css('div.reviewText ::text').extract()
-            review_content = [rc.strip() for rc in review_content if rc.strip()]
-            item['content'] = ', '.join(review_content)
+            created_loc = row.css('span.review-date::text').extract_first().strip().split('in ')[-1]
+            item['location'] = created_loc[4] + created_loc[11] 
+            
+
+            #review_content = row.css('div.reviewText ::text').extract()
+            #review_content = [rc.strip() for rc in review_content if rc.strip()]
+            #item['content'] = ', '.join(review_content)
 
             yield item
